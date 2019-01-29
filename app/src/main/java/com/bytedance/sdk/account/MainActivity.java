54166b2e5a194b82d5b604aa4268b0c0835986b0
changlei <changlei@bytedance.com>
2019-01-29 15:19:06 +0800

    增加bd-opensdk 0.0.2.1版本的新功能的演示代码

--- app/src/main/java/com/bytedance/sdk/account/MainActivity.java
+++ app/src/main/java/com/bytedance/sdk/account/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         request.optionalScope1 = "friend_relation,message";
         request.state = "ww";
         request.wapRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+        // wap预加载接口，需要和sendAuthLogin或者sendInnerWebAuthRequest使用配置相同的SendAuth.Request，但不需要是同一实例
         bdOpenApi.preloadWebAuth(request);
 
         findViewById(R.id.go_to_auth).setOnClickListener(new View.OnClickListener() {
@@ -54,11 +55,11 @@ public class MainActivity extends AppCompatActivity {
         request.scope = "user_info";                            // 用户授权时必选权限
         request.optionalScope1 = "friend_relation,message";     // 用户授权时可选权限（默认不选）
         request.state = "ww";                                   // 用于保持请求和回调的状态，授权请求后原样带回给第三方。
-        request.wapRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+        request.wapRequestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;     // 指定wap授权页横竖屏展示，不指定时由系统控制
         if (isWebAuth) {
-            return bdOpenApi.sendInnerWebAuthRequest(request);
+            return bdOpenApi.sendInnerWebAuthRequest(request);     // 打开wap授权页进行授权
         } else {
-            return bdOpenApi.sendAuthLogin(request);
+            return bdOpenApi.sendAuthLogin(request);               // 优先使用抖音app进行授权，如果抖音app因版本或者其他原因无法授权，则使用wap页授权
         }
     }
 
