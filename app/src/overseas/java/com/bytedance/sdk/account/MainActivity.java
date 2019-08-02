373771162ea80f2e8f76374d4b55fb7d505cd0d5
cassie.wang <cassie.wang@bytedance.com>
2019-08-02 11:07:15 +0800

    demo修改scope，增加新的clientkey

--- app/src/overseas/java/com/bytedance/sdk/account/MainActivity.java
+++ app/src/overseas/java/com/bytedance/sdk/account/MainActivity.java
@@ -62,8 +62,6 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> mUri = new ArrayList<>();
 
     private String mScope = "user_info";
-    private String mOptionalScope1 = "friend_relation";
-    private String mOptionalScope2 = "message";
 
     public static boolean IS_AUTH_BY_M = false;
 
@@ -160,8 +158,6 @@ public class MainActivity extends AppCompatActivity {
     private boolean sendAuth() {
         Authorization.Request request = new Authorization.Request();
         request.scope = mScope;                          // 用户授权时必选权限
-        request.optionalScope1 = mOptionalScope2;     // 用户授权时可选权限（默认选择）
-        request.optionalScope0 = mOptionalScope1;    // 用户授权时可选权限（默认不选）
         request.state = "ww";                                   // 用于保持请求和回调的状态，授权请求后原样带回给第三方。
         return bdOpenApi.authorize(request);               // 优先使用抖音app进行授权，如果抖音app因版本或者其他原因无法授权，则使用wap页授权
 
