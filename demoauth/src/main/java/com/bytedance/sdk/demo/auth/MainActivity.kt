eda0f23e3b22199e15289cb71dc3ab0662692761
Chen Wu <chen.wu@bytedance.com>
2022-07-13 07:20:01 -0700

    init demo auth app

---
+++ demoauth/src/main/java/com/bytedance/sdk/demo/auth/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bytedance.sdk.demo.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
