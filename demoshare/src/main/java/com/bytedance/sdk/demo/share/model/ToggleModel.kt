142266b892d52ec2165544db4947e3b065f4cfe8
young.kim <young.kim@bytedance.com>
2022-10-02 14:08:58 -0700

    "feat: WIP change into mvvm pattern"

--- demoshare/src/main/java/com/bytedance/sdk/demo/share/model/ToggleModel.kt
+++ demoshare/src/main/java/com/bytedance/sdk/demo/share/model/ToggleModel.kt
@@ -3,8 +3,7 @@ package com.bytedance.sdk.demo.share.model
 data class ToggleModel(
     val title: String,
     val desc: String,
-    val isOn: Boolean = false,
-    val onToggleChange: (Boolean) -> Unit
+    val isOn: Boolean = false
 ) : DataModel {
     override val viewType = ViewType.TOGGLE
 }
