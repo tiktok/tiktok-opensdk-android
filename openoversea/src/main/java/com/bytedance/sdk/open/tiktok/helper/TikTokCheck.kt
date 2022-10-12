41e082b6cb11195afb569bd1128efcef7f959371
Haifan Ou <haifan.ou@bytedance.com>
2022-10-12 17:13:17 +0000

    [Bug Fix] fix video upload scope description

--- openoversea/src/main/java/com/bytedance/sdk/open/tiktok/helper/TikTokCheck.kt
+++ openoversea/src/main/java/com/bytedance/sdk/open/tiktok/helper/TikTokCheck.kt
@@ -5,5 +5,5 @@ import com.bytedance.sdk.open.tiktok.base.AppCheckBase
 
 class TikTokCheck(override val context: Context) : AppCheckBase(context) {
     override val signature: String = "aea615ab910015038f73c47e45d21466"
-    override val packageName: String = "com.zhiliaoapp.musically"
+    override val packageName: String = "com.ss.android.ugc.trill"
 }
