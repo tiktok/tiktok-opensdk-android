a064f9c2c69657abf0228e0910627178f648ccd1
haifan.ou <haifan.ou@bytedance.com>
2023-01-03 11:12:25 -0800

    update header

--- demo-auth/src/main/java/com/bytedance/sdk/demo/auth/MainViewModel.kt
+++ demo-auth/src/main/java/com/bytedance/sdk/demo/auth/MainViewModel.kt
@@ -1,20 +1,11 @@
 package com.bytedance.sdk.demo.auth
 
 /*
-    Copyright 2022 TikTok Pte. Ltd.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+ *  Copyright (c)  2022 TikTok Pte. Ltd. All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
@@ -147,6 +138,7 @@ class MainViewModel(
         val request = Auth.Request(
             scope = enabledScopes.joinToString(),
             packageName = packageName,
+            language = "zh-Hant-TW",
             resultActivityFullPath = resultActivityFullPath
         )
         val authType = if (webAuthEnabled) {
--- tiktok-open-sdk-core/src/main/java/com/bytedance/sdk/open/tiktok/core/appcheck/TikTokAppCheckFactory.kt
+++ tiktok-open-sdk-core/src/main/java/com/bytedance/sdk/open/tiktok/core/appcheck/TikTokAppCheckFactory.kt
@@ -1,5 +1,12 @@
 package com.bytedance.sdk.open.tiktok.core.appcheck
 
+/*
+ *  Copyright (c)  2022 TikTok Pte. Ltd. All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import android.content.Context
 import com.bytedance.sdk.open.tiktok.core.constants.Constants
 
